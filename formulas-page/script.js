3258bb74f881e5a3b08d514290005ca031adb01e
Eric Roeters <erroeters@gmail.com>
2022-11-09 13:29:12 -0500

    finished area equations

--- formulas-page/script.js
+++ formulas-page/script.js
@@ -48,3 +48,56 @@ function circle() {
     document.getElementById("cir-area").innerText = pi * radius**2;
 }
 }
+
+// cone equation
+
+function cone() {
+    let radius = document.getElementById("cone-rad").value;
+    let slant = document.getElementById("cone-slant").value;
+
+    if (radius <= 0 || slant <= 0) {
+        alert("Please enter a valid number higher than 0")
+    } else {
+    document.getElementById("cone-area").innerText = pi * radius * slant;
+}
+}
+
+// sphere equation
+
+function sphere() {
+    let radius = document.getElementById("sphe-rad").value;
+
+    if (radius <= 0) {
+        alert("Please enter a valid number higher than 0")
+    } else {
+    document.getElementById("sphe-area").innerText = 4 * pi * radius**2;
+}
+}
+
+// cube equation
+
+function cube() {
+    let edge = document.getElementById("cube-len").value;
+
+    if (edge <= 0) {
+        alert("Please enter a valid number higher than 0")
+    } else {
+    document.getElementById("cube-area").innerText = 6 * edge**2;
+}
+}
+
+
+
+// box volume equation
+
+// function box() {
+//     let length = document.getElementById("box-rad").value;
+//     let width = document.getElementById("box-rad").value;
+//     let height = document.getElementById("box-rad").value;
+
+//     if (length <= 0 || width <= 0 || height <= 0) {
+//         alert("Please enter a valid number higher than 0")
+//     } else {
+//     document.getElementById("box-area").innerText = length * width * height;
+// }
+// }
