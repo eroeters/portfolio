d375482ae6e0e218892380f87dc7fa3c3f2c02ca
Eric Roeters <erroeters@gmail.com>
2022-11-09 16:20:19 -0500

    added more buttons to calculator

---
+++ calculator/script.js
@@ -0,0 +1,15 @@
+const btn1 = document.getElementById("btn1").value;
+const btn2 = document.getElementById("btn2").value;
+const btn3 = document.getElementById("btn3").value;
+const btn4 = document.getElementById("btn4").value;
+const btn5 = document.getElementById("btn5").value;
+const btn6 = document.getElementById("btn6").value;
+const btn7 = document.getElementById("btn7").value;
+const btn8 = document.getElementById("btn8").value;
+const btn9 = document.getElementById("btn9").value;
+const btn0 = document.getElementById("btn0").value;
+
+function f(val) {
+    document.getElementById("disp-content").innerText = val;
+    console.log(val);
+}
