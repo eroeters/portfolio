f460e4291fbf14929ab59acfedf8e76feef52f2f
Eric Roeters <erroeters@gmail.com>
2023-05-03 16:47:03 -0400

    updated js and html files

---
+++ image_generator/script.js
@@ -0,0 +1,253 @@
+// Accessories
+
+const accessories = [
+    {
+    text: "earrings",
+    src: "./alpaca/acessories/earrings.png"
+    },
+    {
+        text: "flower",
+        src: "./alpaca/acessories/flower.png"
+    },
+    {
+        text: "glasses",
+        src: "./alpaca/acessories/glasses.png"
+    },
+    {
+        text: "headphones",
+        src: "./alpaca/acessories/headphones.png"
+    }
+]
+
+// Backgrounds
+
+const backgrounds = [
+    {
+        text: "blue50",
+        src: "./alpaca/backgrounds/blue50.png"
+    },
+    {
+        text: "blue60",
+        src: "./alpaca/backgrounds/blue60.png"
+    },
+    {
+        text: "blue70",
+        src: "./alpaca/backgrounds/blue70.png"
+    },
+    {
+        text: "darkblue30",
+        src: "./alpaca/backgrounds/darkblue30.png"
+    },
+    {
+        text: "darkblue50",
+        src: "./alpaca/backgrounds/darkblue50.png"
+    },
+    {
+        text: "darkblue70",
+        src: "./alpaca/backgrounds/darkblue70.png"
+    },
+    {
+        text: "green50",
+        src: "./alpaca/backgrounds/green50.png"
+    },
+    {
+        text: "green60",
+        src: "./alpaca/backgrounds/green60.png"
+    },
+    {
+        text: "green70",
+        src: "./alpaca/backgrounds/green70.png"
+    },
+    {
+        text: "grey40",
+        src: "./alpaca/backgrounds/grey40.png"
+    },
+    {
+        text: "grey70",
+        src: "./alpaca/backgrounds/grey70.png"
+    },
+    {
+        text: "grey80",
+        src: "./alpaca/backgrounds/grey80.png"
+    },
+    {
+        text: "red50",
+        src: "./alpaca/backgrounds/red50.png"
+    },
+    {
+        text: "red60",
+        src: "./alpaca/backgrounds/red60.png"
+    },
+    {
+        text: "red70",
+        src: "./alpaca/backgrounds/red70.png"
+    },
+    {
+        text: "yellow50",
+        src: "./alpaca/backgrounds/yellow50.png"
+    },
+    {
+        text: "yellow60",
+        src: "./alpaca/backgrounds/yellow60.png"
+    },
+    {
+        text: "yellow70",
+        src: "./alpaca/backgrounds/yellow70.png"
+    }
+]
+
+// ears
+
+const ears = [
+    {
+    text: "default",
+    src: "./alpaca/acessories/default.png"
+    },
+    {
+        text: "tilt-backward",
+        src: "./alpaca/acessories/tilt-backward.png"
+    },
+    {
+        text: "tilt-forward",
+        src: "./alpaca/acessories/tilt-forward.png"
+    }
+]
+
+// eyes
+
+const eyes = [
+    {
+        text: "angry",
+        src: "./alpaca/acessories/angry.png"
+    },
+    {
+        text: "default",
+        src: "./alpaca/acessories/default.png"
+    },
+    {
+        text: "naughty",
+        src: "./alpaca/acessories/naughty.png"
+    },
+    {
+        text: "panda",
+        src: "./alpaca/acessories/panda.png"
+    },
+    {
+        text: "smart",
+        src: "./alpaca/acessories/smart.png"
+    },
+    {
+        text: "star",
+        src: "./alpaca/acessories/star.png"
+    }
+]
+
+// hair
+
+const hair = [
+    {
+        text: "bang",
+        src: "./alpaca/acessories/bang.png"
+    },
+    {
+        text: "curls",
+        src: "./alpaca/acessories/curls.png"
+    },
+    {
+        text: "default",
+        src: "./alpaca/acessories/default.png"
+    },
+    {
+        text: "elegant",
+        src: "./alpaca/acessories/elegant.png"
+    },
+    {
+        text: "fancy",
+        src: "./alpaca/acessories/fancy.png"
+    },
+    {
+        text: "quiff",
+        src: "./alpaca/acessories/quiff.png"
+    },
+    {
+        text: "short",
+        src: "./alpaca/acessories/short.png"
+    }
+]
+
+// leg
+
+const leg = [
+    {
+        text: "bubble-tea",
+        src: "./alpaca/acessories/bubble-tea.png"
+    },
+    {
+        text: "cookie",
+        src: "./alpaca/acessories/cookie.png"
+    },
+    {
+        text: "default",
+        src: "./alpaca/acessories/default.png"
+    },
+    {
+        text: "game-console",
+        src: "./alpaca/acessories/game-console.png"
+    },
+    {
+        text: "tilt-backward",
+        src: "./alpaca/acessories/tilt-backward.png"
+    },
+    {
+        text: "tilt-forward",
+        src: "./alpaca/acessories/tilt-forward.png"
+    }
+]
+
+// mouth
+
+const mouth = [
+    {
+        text: "bang",
+        src: "./alpaca/acessories/bang.png"
+    },
+    {
+        text: "curls",
+        src: "./alpaca/acessories/curls.png"
+    },
+    {
+        text: "default",
+        src: "./alpaca/acessories/default.png"
+    },
+    {
+        text: "elegant",
+        src: "./alpaca/acessories/elegant.png"
+    },
+    {
+        text: "fancy",
+        src: "./alpaca/acessories/fancy.png"
+    }
+]
+
+// neck
+
+const neck = [
+    {
+        text: "bend-backward",
+        src: "./alpaca/acessories/bend-backward.png"
+    },
+    {
+        text: "bend-forward",
+        src: "./alpaca/acessories/bend-forward.png"
+    },
+    {
+        text: "default",
+        src: "./alpaca/acessories/default.png"
+    },
+    {
+        text: "thick",
+        src: "./alpaca/acessories/thick.png"
+    }
+]
+
+// 
